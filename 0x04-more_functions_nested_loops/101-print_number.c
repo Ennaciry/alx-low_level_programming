e7e75f93765d9466fc06a8f8dae0166a8c359385
Ennaciry <fatimaezzahraennaciry4@gmail.com>
2023-02-23 17:45:15 -0800

    1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,26 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
+        unsigned int n1;
 
-	b = 10;
+        if (n < 0)
+        {
+                n1 = -n;
+                _putchar('-');
+        } else
+        {
+                n1 = n;
+        }
 
-	if (n < 10 && n >= 0)
-	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
-	}
+        if (n1 / 10)
+        {
+                print_number(n1 / 10);
+        }
 
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+        _putchar((n1 % 10) + '0');
 }
